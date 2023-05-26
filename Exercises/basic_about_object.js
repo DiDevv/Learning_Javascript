b58a347b1ad9f045708b76a17be51a70f1938efb
Diogo <101913207+DiDevv@users.noreply.github.com>
2023-05-26 11:30:44 -0300

    Create basic_about_object.js

---
+++ Exercises/basic_about_object.js
@@ -0,0 +1,18 @@
+const student = {}
+student.name = "Diogo Dias da Silva"
+student.age = "18"
+student.city = "Goiana"
+student.number = "(81) 9 9103-7951"
+
+const student2 = {
+    name: "Felipe Bezerra da Silva Neto",
+    age: "19",
+    city: "Campina Grande",
+    number: "(83) 9 9857-9083"
+}
+
+if (student.age > student2.age) {
+    console.log("The student 1 is older than student 2")
+} else {
+    console.log("O student 2 is older than student 1 ")
+}
